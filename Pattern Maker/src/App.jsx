199eedcc89afc4babed18165e63ebbb9f997831a
Shingfling <100726546+Shingfling@users.noreply.github.com>
2023-07-22 21:16:01 -0400

    creates generateStitchArray
    by extrapolating this functionality out to its own helper method, we can make the code more readable and also make it easier to change in the future.

--- Pattern Maker/src/App.jsx
+++ Pattern Maker/src/App.jsx
@@ -1,6 +1,36 @@
 import React, { useState } from "react";
 import Section from "./Section";
 
+// Helper function to generate the stitch array for a given stitches string
+const generateStitchArray = (stitches) => {
+  const stitchArray = [];
+  const splitStitches = stitches.split(",");
+
+  splitStitches.forEach((stitch) => {
+    const splitStitch = stitch.split("*");
+    const stitchType = splitStitch[0];
+    let stitchCount = 1; // Default to 1 if no stitch count is provided
+    let instruction = "";
+
+    if (splitStitch.length > 1) {
+      // Check for presence of instruction in square brackets
+      const instructionStart = splitStitch[1].indexOf("[");
+      if (instructionStart !== -1) {
+        stitchCount = parseInt(splitStitch[1].substring(0, instructionStart), 10);
+        const instructionEnd = splitStitch[1].indexOf("]");
+        instruction = splitStitch[1].substring(instructionStart + 1, instructionEnd);
+      } else {
+        stitchCount = parseInt(splitStitch[1], 10);
+      }
+    }
+
+    for (let i = 0; i < stitchCount; i++) {
+      stitchArray.push({ name: stitchType, value: 1, instruction, complete: false });
+    }
+  });
+
+  return stitchArray;
+};
 
 function CrochetPatternGenerator() {
   const [sections, setSections] = useState([{ title: "", rows: [{ stitches: "" }] }]);
@@ -32,115 +62,53 @@ function CrochetPatternGenerator() {
     }
     setSections(newSections);
 
-    setSavedJson(JSON.stringify(
+    // Recalculate the JSON representation after removing the row using the newSections variable
+    const newJson = JSON.stringify(
       {
-        sections: sections.map(({ title, rows }) => ({
+        sections: newSections.map(({ title, rows }) => ({
           title,
-          rows: rows.map(({ stitches }) => {
-            const stitchArray = [];
-            const splitStitches = stitches.split(",");
-    
-            splitStitches.forEach((stitch) => {
-              const splitStitch = stitch.split("*");
-              const stitchType = splitStitch[0];
-              let stitchCount = 1; // Default to 1 if no stitch count is provided
-              let instruction = "";
-    
-              if (splitStitch.length > 1) {
-                // Check for presence of instruction in square brackets
-                const instructionStart = splitStitch[1].indexOf("[");
-                if (instructionStart !== -1) {
-                  stitchCount = parseInt(splitStitch[1].substring(0, instructionStart), 10);
-                  const instructionEnd = splitStitch[1].indexOf("]");
-                  instruction = splitStitch[1].substring(instructionStart + 1, instructionEnd);
-                } else {
-                  stitchCount = parseInt(splitStitch[1], 10);
-                }
-              }
-    
-              for (let i = 0; i < stitchCount; i++) {
-                stitchArray.push({ name: stitchType, value: 1, instruction, complete: false });
-              }
-            });
-            return stitchArray;
-          }),
+          rows: rows.map(({ stitches }) => stitches),
         })),
       },
       null,
       2
-    )
-    )
+    );
+
+    setSavedJson(newJson);
   };
 
   const handleEditRowClick = (sectionIndex, rowIndex) => {
     setEditingRow({ section: sectionIndex, row: rowIndex });
   };
 
- const handleRowInputChange = (event, sectionIndex, rowIndex) => {
-  const newSections = [...sections];
-  newSections[sectionIndex].rows[rowIndex].stitches = event.target.value;
-  setSections(newSections);
-};
+  const handleRowInputChange = (event, sectionIndex, rowIndex) => {
+    const newSections = [...sections];
+    newSections[sectionIndex].rows[rowIndex].stitches = event.target.value;
+    setSections(newSections);
+  };
 
-
-
-
-const handleSaveRowClick = () => {
-  const newSections = [...sections];
-  const { section: sectionIndex, row: rowIndex } = editingRow;
-  const row = newSections[sectionIndex].rows[rowIndex];
-
-  // Split the stitches string into an array
-  const splitStitches = Array.isArray(row.stitches) ? row.stitches.join(",") : row.stitches;
-
-
-  // Replace the stitches string with the new array
-  row.stitches = splitStitches;
-
-  setSavedJson(JSON.stringify(
-    {
-      sections: sections.map(({ title, rows }) => ({
-        title,
-        rows: rows.map(({ stitches }) => {
-          const stitchArray = [];
-          const splitStitches = stitches.split(",");
+  const handleSaveRowClick = () => {
+    const newSections = [...sections];
+    const { section: sectionIndex, row: rowIndex } = editingRow;
+    const row = newSections[sectionIndex].rows[rowIndex];
   
-          splitStitches.forEach((stitch) => {
-            const splitStitch = stitch.split("*");
-            const stitchType = splitStitch[0];
-            let stitchCount = 1; // Default to 1 if no stitch count is provided
-            let instruction = "";
+    // Update the savedJson state using the newSections variable
+    const newJson = JSON.stringify(
+      {
+        sections: newSections.map(({ title, rows }) => ({
+          title,
+          rows: rows.map(({ stitches }) => (stitches === row.stitches ? generateStitchArray(stitches) : stitches)),
+        })),
+      },
+      null,
+      2
+    );
   
-            if (splitStitch.length > 1) {
-              // Check for presence of instruction in square brackets
-              const instructionStart = splitStitch[1].indexOf("[");
-              if (instructionStart !== -1) {
-                stitchCount = parseInt(splitStitch[1].substring(0, instructionStart), 10);
-                const instructionEnd = splitStitch[1].indexOf("]");
-                instruction = splitStitch[1].substring(instructionStart + 1, instructionEnd);
-              } else {
-                stitchCount = parseInt(splitStitch[1], 10);
-              }
-            }
+    setSavedJson(newJson);
   
-            for (let i = 0; i < stitchCount; i++) {
-              stitchArray.push({ name: stitchType, value: 1, instruction, complete: false });
-            }
-          });
-          return stitchArray;
-        }),
-      })),
-    },
-    null,
-    2
-  ));
-
-  setSections(newSections);
-  setEditingRow(null);
-};
-  
-  
-  
+    setSections(newSections);
+    setEditingRow(null);
+  };
 
   const handleSectionTitleChange = (event, sectionIndex) => {
     const newSections = [...sections];
@@ -148,17 +116,8 @@ const handleSaveRowClick = () => {
     setSections(newSections);
   };
 
-
-
-
-
-  
-
   const outputJson = savedJson || JSON.stringify({ sections: [] }, null, 2);
   const outputFormatted = outputJson.replace(/},/g, "},\n").replace(/],/g, "],\n");
-
-
-  
 
   return (
     <div>
